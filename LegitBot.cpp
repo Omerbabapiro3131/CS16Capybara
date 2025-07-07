84fa48afeb2d94225bb65f77c4930af2a88dc85f
Omerbabapiro3131 <omer02072016@gmail.com>
2025-07-07 19:02:35 +0300

    LegitBot.cpp eklendi

---
+++ LegitBot.cpp
@@ -0,0 +1,7 @@
+#include "LegitBot.h"
+#include <iostream>
+
+void RunLegitBot() {
+    std::cout << "[LegitBot] Smooth nişanlama aktif\n";
+    // Burada oyuncu pozisyonu alıp smooth aimbot kodu eklenebilir
+}
